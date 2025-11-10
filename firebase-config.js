dd6c14d9c5fde9df5e24d13becde7c1e7b66fe71
Ravi Jain <ravijain236129@gmail.com>
2025-11-11 04:32:30 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,33 +1,33 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-  import { 
-    getFirestore, collection, addDoc, getDocs, getDoc, 
-    updateDoc, deleteDoc, doc, query, where, orderBy 
-  } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
-  import { 
-    getStorage, ref, uploadBytes, getDownloadURL, deleteObject 
-  } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+// firebase-config.js
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDxNMhU09mINvq_aDLtylBg3FucCK-MzYE",
-    authDomain: "sandstonebijoliya-293d2.firebaseapp.com",
-    projectId: "sandstonebijoliya-293d2",
-    storageBucket: "sandstonebijoliya-293d2.appspot.com",
-    messagingSenderId: "133756247845",
-    appId: "1:133756247845:web:8e769ce2af3db1765484cc",
-    measurementId: "G-H7JMX9VNYF"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { 
+  getFirestore, collection, addDoc, getDocs, getDoc, 
+  updateDoc, deleteDoc, doc, query, where, orderBy 
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+import { 
+  getStorage, ref, uploadBytes, getDownloadURL, deleteObject 
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
 
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const db = getFirestore(app);
-  const storage = getStorage(app);
+const firebaseConfig = {
+  apiKey: "AIzaSyDxNMhU09mINvq_aDLtylBg3FucCK-MzYE",
+  authDomain: "sandstonebijoliya-293d2.firebaseapp.com",
+  projectId: "sandstonebijoliya-293d2",
+  storageBucket: "sandstonebijoliya-293d2.appspot.com",
+  messagingSenderId: "133756247845",
+  appId: "1:133756247845:web:8e769ce2af3db1765484cc",
+  measurementId: "G-H7JMX9VNYF"
+};
 
-  export {
-    db, storage,
-    collection, addDoc, getDocs, getDoc, updateDoc, deleteDoc, doc,
-    query, where, orderBy,
-    ref, uploadBytes, getDownloadURL, deleteObject
-  };
-</script>
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export {
+  db, storage,
+  collection, addDoc, getDocs, getDoc, updateDoc, deleteDoc, doc,
+  query, where, orderBy,
+  ref, uploadBytes, getDownloadURL, deleteObject
+};
